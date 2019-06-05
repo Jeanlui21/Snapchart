e2c0dfdcf1f9ac3b62c1518f59407c3d28440882
Jeanlui21 <jean.ferrer@tecsup.edu.pe>
2019-06-05 17:12:11 -0500

    Subida

---
+++ RedSocial/SnapsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SnapsViewController.swift
+//  RedSocial
+//
+//  Created by Jean Ferrer on 3/6/19.
+//  Copyright © 2019 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    @IBAction func cerrarSesionTapped(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
